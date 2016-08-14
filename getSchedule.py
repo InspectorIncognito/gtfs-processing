03b8a185732b5367af152f9e64d4e2aa1fe96ae7
Mauricio Zuñiga Gajardo <mzuniga@gmail.com>
2016-08-14 01:17:54 -0300

    manejo de errores
    Se agregaron los try para los casos de servicios sin datos

--- getSchedule.py
+++ getSchedule.py
@@ -10,7 +10,19 @@ services =json.load(urllib2.urlopen("http://www.transantiago.cl/restservice/rest
 f = open('horarios.csv', 'w')
 
 for service in services:
-	data =json.load(urllib2.urlopen("http://www.transantiago.cl/restservice/rest/getrecorrido/"+service))
+	webdata = ""
+	try :
+		webdata = urllib2.urlopen("http://www.transantiago.cl/restservice/rest/getrecorrido/"+service)
+	except urllib2.HTTPError:
+		print "Oops, no se pudo recuperar : " + service
+		continue
+
+	try:
+		data =json.load(webdata)
+	except ValueError:
+		print "Oops, no se pudo recuperar : " + service
+		continue
+
 	print data[0]['cod']
 	for horario in data[0]['horarios']:
 		registro = data[0]['cod'] + ";" + data[0]['destino'] + ";" + horario["tipoDia"] + ";" + horario["inicio"] + ";" + horario['fin']
@@ -21,7 +33,7 @@ for service in services:
 		f.write( registro.encode('latin-1') )
 		f.write ("\n")
 	#break;
-	time.sleep( 1 )
+	#time.sleep( 1 )
 	
 #
 
