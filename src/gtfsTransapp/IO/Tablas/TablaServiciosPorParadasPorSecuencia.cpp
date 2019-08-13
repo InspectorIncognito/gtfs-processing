04ae868c6c8fd494fb44612b9e32388294533700
mzunigag <mzuniga@gmail.com>
2019-08-13 17:33:19 -0400

    version linux terminada src/gtfsTransapp

--- src/gtfsTransapp/IO/Tablas/TablaServiciosPorParadasPorSecuencia.cpp
+++ src/gtfsTransapp/IO/Tablas/TablaServiciosPorParadasPorSecuencia.cpp
@@ -263,6 +263,10 @@ void TablaServiciosPorParadasPorSecuencia::Crear()
 		fout << tmp[0] << ";";
 		fout << tmp[1] << ";";
 
+//         cout << (*itseq).second.modo << ";";
+// 		cout << tmp[0] << ";";
+// 		cout << tmp[1] << ";";
+        
 		if (itseq_ant != secuenciasPorHorario.end())
 		{
 			vector<string> tmp_pre = StringFunctions::Explode((*itseq_ant).first, ';');
@@ -273,6 +277,7 @@ void TablaServiciosPorParadasPorSecuencia::Crear()
 			string codigo_pre;
 			string codigo_cur;
 
+            
 			codigo_cur = tmp[0] + "|" + tmp[1] + "|";
 			if (variante1.size() == 2)
 				codigo_cur += variante1[1] ;
@@ -302,18 +307,25 @@ void TablaServiciosPorParadasPorSecuencia::Crear()
 			if (codigo_cur.compare(codigo_pre) == 0)
 			{
 				fout << variante_int++ << ";";
-				//cout << codigo_cur << endl;
 			}
 			else
 			{
 				if (variante1.size() == 2)
+                {
 					fout << variante1[1] << ";";
+                }
 				else if (variante2.size() == 2)
+                {
 					fout << variante2[1] << ";";
+                }
 				else if (variante1.size() == 3)
+                {
 					fout << variante1[1] << "I" << variante1[2] << ";";
+                }
 				else if (variante2.size() == 3)
+                {
 					fout << variante2[1] << "R" << variante2[2] << ";";
+                }
 				else
 					fout << "-" << ";";
 			}
@@ -322,13 +334,21 @@ void TablaServiciosPorParadasPorSecuencia::Crear()
 		else
 		{
 			if (variante1.size() == 2)
+            {
 				fout << variante1[1] << ";";
+            }
 			else if (variante2.size() == 2)
+            {
 				fout << variante2[1] << ";";
+            }
 			else if (variante1.size() == 3)
+            {
 				fout << variante1[1] << "I" << variante1[2] << ";";
+            }
 			else if (variante2.size() == 3)
+            {
 				fout << variante2[1] << "R" << variante2[2] << ";";
+            }
 			else
 				fout << "-" << ";";
 		}
@@ -341,182 +361,6 @@ void TablaServiciosPorParadasPorSecuencia::Crear()
 		fout << tmp[3] << endl;
 	}
 	fout.close();
-
-	/*
-	ofstream fout;
-	fout.open("Android_busstops_sequences" + fdd_->parametros->version + ".csv");
-	fout << "servicio;sentido;tipodia;color_id;horario;destino;direccion;codigo_ruta;paradas" << endl;
-	for (itseq = secuenciasPorHorario.begin(); itseq != secuenciasPorHorario.end(); itseq++)
-	{
-		vector<string> tmp = StringFunctions::Explode((*itseq).first, ';');
-
-		fout << tmp[0] << ";";
-		fout << tmp[1] << ";";
-		fout << tmp[2] << ";";
-		fout << (*itseq).second.color << ";";
-		fout << (*itseq).second.horario << ";";
-		fout << toCamelCase(string((*itseq).second.nombre + ";"));
-		fout << toCamelCase(string((*itseq).second.nombre + " " + tmp[2] + " (" + (*itseq).second.horario + ");"));
-		fout << "-" << ";";
-		fout << tmp[3] << endl;
-	}
-	fout.close();
-	*/
-
-	/*
-	///DEBUG
-	ofstream fout;
-	fout.open("Android_busstops_sequences" + fdd_->parametros->version + ".csv");
-	fout << "servicio;sentido;tipodia;color_id;hora_ini;hora_fin;direccion;paradas" << endl;
-	int min_hora_ini = 999999;
-	int max_hora_fin = -1;
-	for (isec = fdd_->secuencias.begin(); isec != fdd_->secuencias.end(); isec++)
-	{
-		map<string, FuenteDatos::Secuencia >::iterator isec_ant;
-		map<string, FuenteDatos::Secuencia >::iterator isec_sgt;
-		isec_ant = isec;
-		isec_ant--;
-		isec_sgt = isec;
-		isec_sgt++;
-
-
-		if (isec == fdd_->secuencias.begin())
-		{
-			int ihora_ini = fdd_->tsh.Time2Seconds((*isec).second.hora_ini);
-			int ihora_fin = fdd_->tsh.Time2Seconds((*isec).second.hora_fin);
-
-			if (ihora_ini <= min_hora_ini) min_hora_ini = ihora_ini;
-			if (ihora_fin >= max_hora_fin) max_hora_fin = ihora_fin;
-		}
-		else
-			//if (isec_ant != secuencias.end())
-		{
-			vector<string> cod_ant = StringFunctions::Explode((*isec_ant).second.codigo, '-');
-
-			string servicio = cod_ant[0];
-			string sentido = cod_ant[1];
-			string tipoDia = cod_ant[2];
-
-			//chequeo igualdad
-			bool sonIguales = true;
-			if ((int)(*isec).second.paradas.size() != (int)(*isec_ant).second.paradas.size())
-			{
-				sonIguales = false;
-			}
-			else
-			{
-				map<int, string>::iterator ipar1 = (*isec).second.paradas.begin();
-				map<int, string>::iterator ipar2 = (*isec_ant).second.paradas.begin();
-				for (; ipar1 != (*isec).second.paradas.end(); ipar1++, ipar2++)
-				{
-					if ((*ipar1).second.compare((*ipar2).second) != 0)
-						sonIguales = false;
-				}
-			}
-
-			///codigo anterior igual, se fusionan horarios
-			if ((*isec).second.codigo.compare((*isec_ant).second.codigo) == 0 && sonIguales)
-			{
-				int ihora_ini_ant = fdd_->tsh.Time2Seconds((*isec_ant).second.hora_ini);
-				int ihora_fin_ant = fdd_->tsh.Time2Seconds((*isec_ant).second.hora_fin);
-
-				int ihora_ini = fdd_->tsh.Time2Seconds((*isec).second.hora_ini);
-				int ihora_fin = fdd_->tsh.Time2Seconds((*isec).second.hora_fin);
-
-				if (ihora_ini_ant <= min_hora_ini) min_hora_ini = ihora_ini_ant;
-				if (ihora_ini <= min_hora_ini) min_hora_ini = ihora_ini;
-
-				if (ihora_fin_ant >= max_hora_fin) max_hora_fin = ihora_fin_ant;
-				if (ihora_fin >= max_hora_fin) max_hora_fin = ihora_fin;
-			}
-			else
-			{
-				string nombre = string("-");
-				map< string, Servicio >::iterator iserv = fdd_->servicios.find(servicio);
-				if (iserv != fdd_->servicios.end())
-				{
-					if (sentido.compare("I") == 0)
-						nombre = (*iserv).second.destino;
-					else
-						nombre = (*iserv).second.origen;
-				}
-
-
-				if (min_hora_ini == 999999 || max_hora_fin == -1)
-				{
-					//fout << (*isec_ant).first << ";";
-					fout << servicio << ";";
-					fout << sentido << ";";
-					fout << tipoDia << ";";
-
-					map<string, string>::iterator iiit = fdd_->dicSS.colores.find((*iserv).first);
-					if (iiit != fdd_->dicSS.colores.end())
-						fout << (*iiit).second << ";";
-					else
-					{
-						cout << "ERROR : No se encontro el servicio " << (*iserv).first << " en la tabla de colores." << endl;
-						fout << "0" << ";";
-					}
-
-					fout << (*isec_ant).second.hora_ini << ";";
-					fout << (*isec_ant).second.hora_fin << ";";
-					fout << nombre << " " << tipoDia << " (" << (*isec_ant).second.hora_ini << " - " << (*isec_ant).second.hora_fin << ")" << ";";
-					for (map<int, string>::iterator ipar = (*isec_ant).second.paradas.begin(); ipar != (*isec_ant).second.paradas.end(); ipar++)
-					{
-						if (ipar == (*isec_ant).second.paradas.begin())
-							fout << (*ipar).second;
-						else
-							fout << "-" << (*ipar).second;
-					}
-					fout << endl;
-				}
-				else
-				{
-					//fout << (*isec_ant).first << ";";
-					fout << servicio << ";";
-					fout << sentido << ";";
-					fout << tipoDia << ";";
-					map<string, string>::iterator iiit = fdd_->dicSS.colores.find((*iserv).first);
-					if (iiit != fdd_->dicSS.colores.end())
-						fout << (*iiit).second << ";";
-					else
-					{
-						cout << "ERROR : No se encontro el servicio " << (*iserv).first << " en la tabla de colores." << endl;
-						fout << "0" << ";";
-					}
-
-					fout << fdd_->tsh.Seconds2TimeStampInDay(min_hora_ini) << ";";
-
-					if (max_hora_fin == 86400)
-					{
-						fout << fdd_->tsh.Seconds2TimeStampInDay(max_hora_fin - 1) << ";";
-						fout << nombre << " " << tipoDia << " (" << fdd_->tsh.Seconds2TimeStampInDay(min_hora_ini) << " - " << fdd_->tsh.Seconds2TimeStampInDay(max_hora_fin - 1) << ")" << ";";
-					}
-					else
-					{
-						fout << fdd_->tsh.Seconds2TimeStampInDay(max_hora_fin) << ";";
-						fout << nombre << " " << tipoDia << " (" << fdd_->tsh.Seconds2TimeStampInDay(min_hora_ini) << " - " << fdd_->tsh.Seconds2TimeStampInDay(max_hora_fin) << ")" << ";";
-					}
-
-
-					for (map<int, string>::iterator ipar = (*isec_ant).second.paradas.begin(); ipar != (*isec_ant).second.paradas.end(); ipar++)
-					{
-						if (ipar == (*isec_ant).second.paradas.begin())
-							fout << (*ipar).second;
-						else
-							fout << "-" << (*ipar).second;
-
-					}
-					fout << endl;
-				}
-
-				min_hora_ini = 999999;
-				max_hora_fin = -1;
-			}
-		}
-	}
-	fout.close();
-	*/
 
 
 	cout << Cronometro::GetMilliSpan( nTimeStart )/60000.0 << "(min)" << endl;
